0a2c1359252f32b37c537da1aef2746105f316b4
JonR98 <joni.repo@student.hamk.fi>
2022-05-02 13:11:33 +0300

    jotain

---
+++ toimisko14/src/main/java/app/Addkysymys2.java
@@ -0,0 +1,40 @@
+package app;
+
+import java.io.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import app.model.Kysymykset;
+
+@WebServlet(
+    name = "Addkysymykset",
+    urlPatterns = {"/addkysymykset"}
+)
+public class Addkysymys2 extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/html");
+    response.setCharacterEncoding("UTF-8");
+
+    PrintWriter out=response.getWriter();
+    out.println("Adding a fish");
+    Kysymykset kysymykset=new Kysymykset();
+    //Create an EntityManagerFactory by the name of persistence unit - see persistence.xml
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("jpa247");
+	//And then EntityManager, which can manage the entities.
+	EntityManager em=emf.createEntityManager();
+	//When using default (RESOURCE-LOCAL) transaction type
+	//Every transaction must begin and end.
+	em.getTransaction().begin();
+	em.persist(kysymykset);
+	em.getTransaction().commit();
+  }
+}
--- toimisko14/src/main/java/app/model/Kysymykset.java
+++ toimisko14/src/main/java/app/model/Kysymykset.java
@@ -1,5 +1,11 @@
 package app.model;
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+@Entity
 public class Kysymykset implements Serializable{
 	
 
